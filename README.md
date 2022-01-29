bbc998b6dc913152d2d3b3969d6b637ee073363f
Kseniya Polyakova <polyakova.kse@yandex.ru>
2022-01-29 22:12:10 +0300

    crud full example

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Start a project in development mode: `npm start`
 
 On pre-commit the automation fires `prettier` и `eslint` tasks. See `int-staged` section in `./package.json`
 
+Graphql Ui sandbox: `http://localhost:8080/graphql`
+
 ## Server
 ### Make a migration
 `cd server/db && npx mikro-orm migration:create -p migrations`
--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,11 +1,12 @@
 import {
-    Arg, Ctx, Query, Resolver, Int,
+    Arg, Ctx, Query, Resolver, Mutation,
 } from 'type-graphql';
 import { Example } from '../entities/Example';
 import { Context } from '../../@types';
 
 @Resolver()
 export class PostResolver {
+    // query all (no pagination)
     @Query(() => [Example])
     examples(
         @Ctx() ctx: Context,
@@ -13,11 +14,52 @@ export class PostResolver {
         return ctx.em.find(Example, {});
     }
 
+    // query single by id
     @Query(() => Example, { nullable: true })
     example(
-        @Arg('id', () => Int) id: number,
+        @Arg('id') id: number,
         @Ctx() ctx: Context,
     ): Promise<Example | null> {
         return ctx.em.findOne(Example, { id });
+    }
+
+    // create new
+    @Mutation(() => Example)
+    async createExample(
+        @Arg('title') title: string,
+        @Ctx() ctx: Context,
+    ): Promise<Example> {
+        const example = ctx.em.create(Example, { title });
+        await ctx.em.persistAndFlush(example);
+        return example;
+    }
+
+    // update by id
+    @Mutation(() => Example, { nullable: true })
+    async updateExample(
+        @Arg('id') id: number,
+        @Arg('title', { nullable: true }) title: string, // makes title possible to be null
+        @Ctx() ctx: Context,
+    ): Promise<Example | null> {
+        const target = await ctx.em.findOne(Example, { id });
+        if (!target) {
+            return null;
+        }
+
+        target.title = title;
+        await ctx.em.persistAndFlush(target);
+
+        return target;
+    }
+
+    // delete by id
+    @Mutation(() => Boolean, { nullable: true })
+    async deleteExample(
+        @Arg('id') id: number,
+        @Ctx() ctx: Context,
+    ): Promise<boolean> {
+        // no try/catch because nativeDelete goes well if there if no such entity found
+        await ctx.em.nativeDelete(Example, { id });
+        return true;
     }
 }
